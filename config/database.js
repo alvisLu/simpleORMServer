7d4a8a378fdf42d103538ffa77e1f253e8e2ee17
Alvis Lu <uualvislu@gmail.com>
2020-12-16 17:57:33 +0800

    fixed config of database

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize('db', 'postgres', 'postgres', {
-  host: 'localhost',
-  port: '5435',
+  host: 'simpleormserver_db_1',
+  port: '5432',
   dialect: 'postgres',
   define: {
     timestamps: false, // disable createAt and updateAt
